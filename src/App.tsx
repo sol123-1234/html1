47ead23833c40584b70bb174301850c60f2ded7f
Jeanice Jones <56509810+jibo001@users.noreply.github.com>
2023-11-19 13:48:12 +0800

    remove loading

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import { RouterProvider } from 'react-router-dom'
 import Web3Wrapper from './container/Web3Wrapper'
 import ReactQueryWrapper from './container/ReactQueryWrapper'
 import router from './router'
-import SuspenseLoading from './components/SuspenseLoading'
 
 function App() {
   return (
-    <React.Suspense fallback={<SuspenseLoading />}>
+    <React.Suspense fallback={<div />}>
       <Web3Wrapper>
         <ReactQueryWrapper>
           <RouterProvider router={router} />

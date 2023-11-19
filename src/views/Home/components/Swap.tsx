0d5a840fe125408bded73a5137f7f16baa2e0b56
Jeanice Jones <56509810+jibo001@users.noreply.github.com>
2023-11-19 13:45:55 +0800

    swap icon

--- src/views/Home/components/Swap.tsx
+++ src/views/Home/components/Swap.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import b from '@/assets/b.png'
+import exchange from '@/assets/swapChange.png'
 
 const Swap = () => {
   const [selectedTab] = useState(0)
@@ -21,8 +22,11 @@ const Swap = () => {
     <div className="p-5 bg-[#fff] rounded-xl  mt-8 lg:mt-[150px] shadow-md mb-5">
       <SwapTab tabs={tabs} selectedTab={selectedTab} />
       <InputField symbol="USDT" className="mt-5" />
-      <InputField symbol="AUSD" className="mt-3" />
-      <div className="flex items-center justify-between text-base text-[#333333] mt-8">
+      <div className="flex justify-end pr-5 -mt-2">
+        <img className="w-7 lg:w-10" src={exchange} alt="" />
+      </div>
+      <InputField symbol="AUSD" className="-mt-3" />
+      <div className="flex items-center justify-between text-xs lg:text-base text-[#333333] mt-8">
         <div>结算期</div>
         <div className="flex items-center justify-end gap-2">
           <div className="flex items-center justify-center px-1 bg-primary rounded-xl">即时</div>
@@ -41,7 +45,7 @@ const Swap = () => {
 
 const SwapTab: React.FC<{ tabs: any[]; selectedTab: number }> = ({ tabs, selectedTab }) => {
   return (
-    <div className="bg-[#292929] text-sm lg:text-xl p-2 rounded-xl justify-between flex items-center">
+    <div className="bg-[#292929] text-sm lg:text-xl p-1 lg:p-2 rounded-xl justify-between flex items-center">
       {tabs.map((item) => (
         <div
           className={`flex-1 text-center py-3 ${selectedTab === item.id && 'bg-primary rounded-xl text-[#000]'}`}
@@ -57,12 +61,12 @@ const SwapTab: React.FC<{ tabs: any[]; selectedTab: number }> = ({ tabs, selecte
 const InputField: React.FC<{ symbol: string; className?: string }> = ({ symbol, className }) => {
   return (
     <div className={`bg-[#f0f0f0] p-5 rounded-xl ${className || ''}`}>
-      <div className="text-base text-[#989898]">余额： 0.00000</div>
+      <div className=" text-[#989898] text-xs lg:text-base">余额： 0.00000</div>
       <div className="flex items-center justify-between mt-2">
         <input type="number" className="flex-1 bg-transparent outline-none" />
         <div className="flex items-center gap-2">
-          <img className="w-[22px]" src={b} alt="" />
-          <div className="text-[#292929] text-xl">{symbol}</div>
+          <img className="w-4 lg:w-[22px]" src={b} alt="" />
+          <div className="text-[#292929] text-sm lg:text-xl">{symbol}</div>
         </div>
       </div>
     </div>
